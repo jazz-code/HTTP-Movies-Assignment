75ade500bad9b3a6b3d837f4f323bb0eb2afdf9c
ant <ren.meta@gmail.com>
2019-08-22 17:16:56 -0400

    update form displaying

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,179 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const initialItem = {
+  name: '',
+  price: '',
+  imageUrl: '',
+  description: '',
+  shipping: ''
+};
+
+const UpdateMovie = props => {
+  const [item, setItem] = useState(initialItem);
+//   console.log(props.movie)
+  useEffect(() => {
+    const id = props.match.params.id;
+    const itemInArr = props.movie.find(item => {
+        console.log(item.id, id)
+        return `${item.id}` === id});
+    if (itemInArr) setItem(itemInArr);
+  }, [props.movie, props.match.params.id]);
+
+  const changeHandler = e => {
+    e.persist();
+    let value = e.target.value;
+    if (e.target.name === 'price') {
+      value = parseInt(value, 10);
+    }
+
+    setItem({
+      ...item,
+      [e.target.name]: value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // axios
+    //   .put(`http://localhost:5000/update-movie/${props.movie.id}`, props.movie)
+    //   .then(res => {
+    //     console.log(res);
+    //     setItem(initialItem);
+    //     props.updateItems(res.data);
+    //     props.history.push('/movies');
+    //   })
+    //   .catch(err => console.log(err.response));
+  };
+
+  return (
+    <div>
+      <h2>Update Movie</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          placeholder="title"
+          value={props.movie.title}
+        />
+
+        <input
+        type="string"
+          name="director"
+          onChange={changeHandler}
+          placeholder="director"
+          value={props.movie.director}
+        />
+
+        <input
+          type="number"
+          name="metascore"
+          onChange={changeHandler}
+          placeholder="metascore"
+          value={props.movie.metascore}
+        />
+
+        <input
+            type="string"
+          name="stars"
+          onChange={changeHandler}
+          placeholder="stars"
+          value={props.movie.stars}
+        />
+
+        <button className="">Update</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateMovie;
+
+
+
+
+
+
+
+// import React, {useState, useEffect} from 'react';
+// import axios from 'axios';
+// import { Form, Field, withFormik } from 'formik';
+// import * as Yup from 'yup';
+
+// const UpdateMovie = ({ errors, touched, values, handleSubmit, status, ...props }) => {
+//   const [item, setItem] = useState({});
+//   console.log(item);
+
+//   useEffect(() => {
+//     if (status) {
+//       setItem([...item, status]);
+//     }
+//   }, [status]);
+
+// //   useEffect(() => {
+// //     const id = props.match.params.id;
+// //     const itemInArr = props.movies.find(item => `${item.id}` === id);
+// //     if (itemInArr) setItem(itemInArr);
+// //   }, [props.movies, props.match.params.id]);
+
+//   return (
+//     <div className="form">
+//       <h1>Update Movie</h1>
+//       <Form>
+//         <Field type="text" name="title" placeholder="title" />
+//         {touched.title && errors.title && (
+//           <p className="error">{errors.title}</p>
+//         )}
+
+//         <Field type="text" name="director" placeholder="director" />
+//         {touched.director && errors.director && <p className="error">{errors.director}</p>}
+
+//         <Field type="text" name="metascore" placeholder="metascore" />
+//         {touched.metascore && errors.metascore && <p className="error">{errors.metascore}</p>}
+
+//         <Field type="text" name="stars" placeholder="stars" />
+//         {touched.stars && errors.stars && <p className="error">{errors.stars}</p>}
+
+//         <button type="submit">Update</button>
+//       </Form>
+
+     
+//     </div>
+//   );
+// };
+
+// const FormikUpdateMovie = withFormik({
+//   mapPropsToValues({ title, director, metascore, stars }) {
+//     return {
+//       title: title || '',
+//       director: director || '',
+//       metascore: metascore || '',
+//       stars: stars || "",
+//     };
+//   },
+
+//   validationSchema: Yup.object().shape({
+//     title: Yup.string().required('Need A Title'),
+//     director: Yup.string().required(),
+//     metascore: Yup.string().required(),
+//     stars: Yup.string().required(),
+//   }),
+
+//   handleSubmit(item, id, {...props}, { setStatus }) {
+//     axios
+//     .put(`https://localhost:3000/api/movie/0`, item)
+//     .then(res => {
+//       console.log(setStatus);
+//       setStatus(item);
+//       props.updateItems(res.data);
+//       props.history.push('/movies');
+//     })
+//     .catch(err => console.log(err.response));
+//     }
+// })(UpdateMovie); 
+
+// export default FormikUpdateMovie;
+
+
+
