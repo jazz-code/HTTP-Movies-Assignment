a707eec6f517311e3eeaaf96100c6491e7558ddf
ant <ren.meta@gmail.com>
2019-08-23 10:28:42 -0400

    PUT request working

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,103 +1,181 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const initialItem = {
-  id: '',  
-  title: '',
-  director: '',
-  metascore: '',
-  stars: [],
-};
+const UpdateMovie = (props) => {
+  console.log(props);
+  const [movie, setMovie] = useState(null);
 
-const UpdateMovie = props => {
-  const [item, setItem] = useState({});
-//   const [stars, setStars]
-//    console.log(item)
-  useEffect(() => {
-    const id = props.match.params.id;
-    const itemInArr = props.item.find(item => {
-        console.log(item.id, id)
-        return `${item.id}` === id});
-    if (itemInArr) setItem(itemInArr);
-  }, [props.item, props.match.params.id]);
-
-  const changeHandler = e => {
-    e.persist();
-    let value = e.target.value
-    setItem({
-      ...item,
-      [e.target.name]: value,
-    //   starsHandler(idx, e)
-    });
+  const fetchMovie = id => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(res => setMovie(res.data))
+      .catch(err => console.log(err.response));
   };
 
-//   const starsHandler = e => {
-//       e.preventDefault();
-//       const setStars = [...item.stars]
-//       setStars[idx] = e.target.value
-//       setItem({
-//           item,
-//           stars: setStars
-//       })
-//   }
+  useEffect(() => {
+    fetchMovie(props.match.params.id);
+  }, [props.match.params.id]);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    axios
-      .put(`http://localhost:5000/api/movies/${item.id}`, item)
+  const handleChange = e => setMovie({...movie, [e.target.name]: e.target.value});
+
+  const handleStar = index => e => {
+    setMovie({...movie, stars: movie.stars.map((star, starIndex) => {
+      return starIndex === index ? e.target.value : star;
+    })});
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.put(`http://localhost:5000/api/movies/${movie.id}`, movie)
       .then(res => {
-        console.log("put request",res, item);
-        setItem(initialItem);
-        console.log("setItem", props.setItem)
-        props.setItem(res.data);
-        // props.history.push(`/api/movies/${item.id}`);
+        console.log(res);
+        props.history.push('/');
       })
       .catch(err => console.log(err.response));
   };
 
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h2>Update Movie</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          onChange={changeHandler}
-          placeholder="title"
-          value={item.title}
-        />
-
-        <input
-        type="string"
-          name="director"
-          onChange={changeHandler}
-          placeholder="director"
-          value={item.director}
-        />
-
-        <input
-          type="number"
-          name="metascore"
-          onChange={changeHandler}
-          placeholder="metascore"
-          value={item.metascore}
-        />
-
-        <input
-            type="string"
-          name="stars"
-          onChange={changeHandler}
-          placeholder="stars"
-          value={item.stars}
-        />
-
-        <button className="">Update</button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+             name="title"
+             placeholder="title"
+             value={movie.title}
+             onChange={handleChange} />
+      <input type="text"
+             name="director"
+             placeholder="director"
+             value={movie.director}
+             onChange={handleChange} />
+      <input type="text"
+             name="metascore"
+             placeholder="metascore"
+             value={movie.metascore}
+             onChange={handleChange} />
+      {movie.stars.map((starName, index) => {
+        return <input type="text"
+                      placeholder="star"
+                      value={starName}
+                      key={index}
+                      onChange={handleStar(index)} />;
+      })}
+      <button type="submit">Update Movie</button>
+    </form>
   );
 };
 
 export default UpdateMovie;
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import Movie from './Movie';
+
+// const initialItem = {
+//   id: '',  
+//   title: '',
+//   director: '',
+//   metascore: '',
+//   stars: [],
+// };
+
+// const UpdateMovie = props => {
+//   // const [item, setItem] = useState({});
+// //   const [stars, setStars]
+// //    console.log(item)
+//   // useEffect(() => {
+//   //   const id = props.match.params.id;
+//   //   const itemInArr = props.item.find(item => {
+//   //       console.log(item.id, id)
+//   //       return `${item.id}` === id});
+//   //   if (itemInArr) setItem(itemInArr);
+//   // }, [props.item, props.match.params.id]);
+//   const [item, setItem] = useState({});
+
+//   const fetchMovie = id => {
+//     axios
+//       .get(`http://localhost:5000/api/movies/${id}`)
+//       .then(res => setItem(res.data))
+//       .catch(err => console.log(err.response));
+//   };
+
+//   useEffect(() => {
+//     fetchMovie(props.match.params.id);
+//   }, [props.match.params.id]);
+ 
+//   const changeHandler = e => setItem({...item, [e.target.name]: e.target.value});
+
+//   const starsHandler = index => e => {
+//     setItem({...item, stars: item.stars.map((star, starIndex) => {
+//       return starIndex === index ? e.target.value : star;
+//     })})
+// }
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     axios
+//       .put(`http://localhost:5000/api/movies/${item.id}`, item)
+//       .then(res => {
+//         console.log("put request",res, item);
+//         setItem(initialItem);
+//         console.log("setItem", setItem)
+//         setItem(res.data);
+//         // props.history.push(`/api/movies/${item.id}`);
+//       })
+//       .catch(err => console.log(err.response));
+//   };
+
+//   return (
+//     <div>
+//       <h2>Update Movie</h2>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           name="title"
+//           onChange={changeHandler}
+//           placeholder="title"
+//           value={item.title}
+//         />
+
+//         <input
+//         type="string"
+//           name="director"
+//           onChange={changeHandler}
+//           placeholder="director"
+//           value={item.director}
+//         />
+
+//         <input
+//           type="number"
+//           name="metascore"
+//           onChange={changeHandler}
+//           placeholder="metascore"
+//           value={item.metascore}
+//         />
+
+// {item.stars.map((starName, index) => {
+//         return <input type="text"
+//                       placeholder="star"
+//                       value={starName}
+//                       key={index}
+//                       onChange={starsHandler(index)} />;
+//       })}
+
+
+//         <button className="">Update</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default UpdateMovie;
 
 
 
